6879675432841d3642910bf35bbe444b2f76644a
Wanderson <30499885+wandersonzz@users.noreply.github.com>
2021-10-15 21:39:06 -0300

    Primeiro commit eclicPRO

---
+++ src/main/java/br/com/eclictechsistemas/eclicpro/EclicProApplication.java
@@ -0,0 +1,13 @@
+package br.com.eclictechsistemas.eclicpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclicProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclicProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eclictechsistemas/eclicpro/EclicProApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eclictechsistemas.eclicpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclicProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
